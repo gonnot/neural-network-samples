3f0b1cec2935e042e754d1db497ed80f980fa715
gonnot <gonnot@yahoo.com>
2016-06-08 23:21:01 +0200

    start MatrixLoader

---
+++ src/main/java/net/gonnot/neuralnetwork/MatrixLoader.java
@@ -0,0 +1,32 @@
+package net.gonnot.neuralnetwork;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+/**
+ *
+ */
+public class MatrixLoader {
+    private MatrixLoader() {
+    }
+
+
+    public static Double[] load(Path path) throws IOException {
+        try (Stream<String> stream = Files.lines(path)) {
+            return streamToMatrix(stream);
+        }
+    }
+
+
+    public static Double[] streamToMatrix(Stream<String> stream) {
+        return stream
+              .map(String::trim)
+              .map(MatrixLoader::toDouble)
+              .toArray(Double[]::new);
+    }
+
+
+    static double toDouble(String doubleInString) {
+        return Double.parseDouble(doubleInString);
+    }
+}
---
+++ src/test/java/net/gonnot/neuralnetwork/MatrixLoaderTest.java
@@ -0,0 +1,40 @@
+package net.gonnot.neuralnetwork;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+/**
+ *
+ */
+public class MatrixLoaderTest {
+
+    @Test
+    public void testFileStreamToMatrix() throws Exception {
+        // todo to-be-removed
+        Double[] matrix = MatrixLoader.load(Paths.get("data/matrix-y.txt"));
+
+        assertThat(matrix).containsOnly(10., 1., 2., 3., 4., 5., 6., 7., 8., 9.);
+    }
+
+
+    @Test
+    public void testGivenTwoLinesWhenStreamToMatrixThenGetVector() throws Exception {
+        Stream<String> stream = Stream.of("0.0", " 1.0 ");
+        Double[] matrix = MatrixLoader.streamToMatrix(stream);
+
+        assertThat(matrix).containsExactly(0.0, 1.0);
+    }
+
+
+    @Test
+    public void testConvertToDouble() throws Exception {
+        assertThat(MatrixLoader.toDouble("1.00000000e+01")).isEqualTo(10.);
+        assertThat(MatrixLoader.toDouble("1.00000000e+00")).isEqualTo(1.);
+    }
+
+
+    @Test(expected = NumberFormatException.class)
+    public void testGivenBadValueWhenConvertToDoubleThenFails() throws Exception {
+        assertThat(MatrixLoader.toDouble("not a double")).isEqualTo(10.);
+    }
+}
